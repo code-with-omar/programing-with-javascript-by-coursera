25ac797c04cf8a9b9335f07278a44e42f1b2d9b9
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-09 13:05:25 +0600

    Complete the fourth number task of Exercise: Advanced use of operators

--- week-1/operator.js
+++ week-1/operator.js
@@ -72,3 +72,10 @@ let result1=test1==0;
 let result2=test2==0;
 console.log("Is" + num1 +" an even number? " + result1);
 console.log("Is" + num2 +" an even number? " + result2);
+
+//  Task 4: Add numbers using the + operator
+/**
+Console log the result of adding two numbers, 5 and 10, using the + operator.
+Note: This task should be completed on a single line of code. The output in the console should be 15.
+ */
+console.log(5+10);
