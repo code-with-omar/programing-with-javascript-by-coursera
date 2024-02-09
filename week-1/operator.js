8047a74af7532b2a4df1eebf777f71ceae0bfb4f
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-09 13:10:38 +0600

    Complete the six number task of Exercise: Advanced use of operators

--- week-1/operator.js
+++ week-1/operator.js
@@ -97,3 +97,18 @@ let now="Now in ";
 let three=3;
 let d="D!";
 console.log(now+three+d);
+
+// Task 6: Use the += operator to accumulate values in a variable
+/**
+Code a new variable and name it counter, assigning it to the value of 0.
+
+On the next line, use the += operator to increase the value of counter by 5.
+
+On the next line, use the += operator to increase the value of counter by 3.
+
+On the fourth line, console log the value of the counter variable.
+ */
+let counter=0;
+counter+=5;
+counter+=3;
+console.log(counter);
