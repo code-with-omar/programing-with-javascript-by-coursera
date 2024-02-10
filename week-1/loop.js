2cee8f64a9c71f54be577ec54e9711cac8ea107f
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-10 11:07:40 +0600

    Complete the 2nd task of Repetitive tasks with loops

--- week-1/loop.js
+++ week-1/loop.js
@@ -12,3 +12,18 @@ for(let i=1;i<=5;i++){
     console.log(i);
 }
 console.log("Counting completed!");
+//task 2
+// Write a "for" loop that will perform exactly the same repetitive code as this:
+/*
+5
+4
+3
+2
+1
+Countdown finished!
+*/
+
+for(let i=5;i>=1;i--){
+    console.log(i);
+}
+console.log("Countdown finished!");
