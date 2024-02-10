ea2e50f1bd39ed144dd7ea85a4cd6559a1c3e8e2
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-10 11:03:13 +0600

    complete the tash one in Exercise: Repetitive tasks with loops

---
+++ week-1/loop.js
@@ -0,0 +1,14 @@
+/*Task 1
+Write a "for" loop that will perform exactly the same repetitive code as this:*
+1
+2
+3
+4
+5
+Counting completed!
+
+ */
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+console.log("Counting completed!");
