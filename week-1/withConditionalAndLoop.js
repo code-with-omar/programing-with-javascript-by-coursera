f7b815337c056a24dc415c8826a9f596a48d9183
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-10 14:26:33 +0600

    Complete the second  exercise of Working with conditionals and loops

--- week-1/withConditionalAndLoop.js
+++ week-1/withConditionalAndLoop.js
@@ -29,14 +29,36 @@ Bronze medal
 9
 10
  */
-for (let i=1;i<=10;i++){
-    if(i===1){
+for (let i = 1; i <= 10; i++) {
+    if (i === 1) {
         console.log("Gold medal");
-    }else if (i===2){
+    } else if (i === 2) {
         console.log("Silver medal");
-    }else if(i===3){
+    } else if (i === 3) {
         console.log("Bronze medal");
-    }else{
+    } else {
         console.log(i);
+    }
+}
+/**
+ * Exercise 2. Use the completed code from the previous task, but convert the conditionals to a switch statement.
+When you code the solution, the output in the console should remain exactly the same as in the previous question.
+
+Note: You'll need three separate cases for the three medals, and a default case for all other values of the i variable.
+ */
+for (var i = 1; i <= 10; i++) {
+    switch(i) {
+        case 1:
+            console.log("Gold medal")
+            break
+        case 2:
+            console.log("Silver medal")
+            break
+        case 3:
+            console.log("Bronze medal")
+            break
+        default:
+            //this block will run if no condition matches
+            console.log(i)
     }
 }
