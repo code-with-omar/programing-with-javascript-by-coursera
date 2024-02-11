b0df92e195d03fabeecb165210747decca1fdfd0
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-11 16:48:36 +0600

    Learn strings are iterable too!

--- week-2/closerLookStrings.js
+++ week-2/closerLookStrings.js
@@ -1,7 +1,16 @@
 //Array iterable
-var veggies = ['onion', 'parsley', 'carrot'];
-console.log(veggies[0]);
-console.log(veggies[1]);
-for (var i = 0; i < veggies.length; i++) {
-    console.log(veggies[i]);
+// var veggies = ['onion', 'parsley', 'carrot'];
+// console.log(veggies[0]);
+// console.log(veggies[1]);
+// for (var i = 0; i < veggies.length; i++) {
+//     console.log(veggies[i]);
+// }
+
+//Strings are iterable, too! 
+let greeting = "Howdy";
+console.log(greeting.length);
+console.log(greeting[0]);
+console.log(greeting[1]);
+for (let i = 0; i < greeting.length; i++) {
+    console.log(greeting[i]);
 }
