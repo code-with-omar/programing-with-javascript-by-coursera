f9148c27604dd959be47b558b1921d6b638fca0a
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-11 17:30:16 +0600

    To learn object method

---
+++ week-2/objectMethods.js
@@ -0,0 +1,23 @@
+// You might already be familiar with objects in JavaScript.
+
+// In this video, you will learn how to design objects as combinations of data and functionality.
+
+// As you might already know, an object consists of key-value pairs, known as properties.
+
+// We can add new key-value pairs to existing objects using the dot notation and the assignment operator. 
+var car = {};
+car.color = "red";
+car.mileage = 98765;
+car.color = "red";
+console.log(car);
+//update the value of a property of the car objject;
+// These are known as properties, and can take many data types, including functions.
+
+//here include a function and function name is turnKey
+car.turnKey = function () {
+    console.log('engine running');
+}
+
+
+console.log(car);
+car.turnKey()//called the turnKey function in car object
