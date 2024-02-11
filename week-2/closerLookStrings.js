47e932d71d244b5820897e7866c7f6b280dbd98e
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-11 16:54:52 +0600

    Learn or try to understand string are not array

--- week-2/closerLookStrings.js
+++ week-2/closerLookStrings.js
@@ -1,10 +1,10 @@
 //Array iterable
-// var veggies = ['onion', 'parsley', 'carrot'];
-// console.log(veggies[0]);
-// console.log(veggies[1]);
-// for (var i = 0; i < veggies.length; i++) {
-//     console.log(veggies[i]);
-// }
+var veggies = ['onion', 'parsley', 'carrot'];
+console.log(veggies[0]);
+console.log(veggies[1]);
+for (var i = 0; i < veggies.length; i++) {
+    console.log(veggies[i]);
+}
 
 //Strings are iterable, too! 
 let greeting = "Howdy";
@@ -14,3 +14,9 @@ console.log(greeting[1]);
 for (let i = 0; i < greeting.length; i++) {
     console.log(greeting[i]);
 }
+//Strings !==arrays
+var greet = "Hello ";
+var user = "lisa";
+console.log(greet.pop());// here error is strings are not array. So we cannot push() and pop()
+console.log(greet + user);
+console.log(greet.concat(user))
