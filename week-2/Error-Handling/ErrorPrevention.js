dbbd264606a78fb1a2d9c0a8182592fd0b75cd04
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-11 21:53:47 +0600

    Complete the 3rd and 4th task of Error prevention

--- week-2/Error-Handling/ErrorPrevention.js
+++ week-2/Error-Handling/ErrorPrevention.js
@@ -11,4 +11,48 @@ function addTwoNums(a, b) {
 Invoke the addTwoNums function with a number and a string
 You need to invoke the addTwoNums using the following arguments: 5 and "5".
 */
-addTwoNums(5,"5");
+addTwoNums(5, "5");
+// Task 3: 
+/**
+ Add the try and catch blocks inside the function definition's body. For now, just make sure that the console log of a + b is inside the try block. Additionally, the catch block should catch an error named err and, inside the body of the catch block, you need to console log the err value.
+ */
+function addTwoNums(a, b) {
+    try {
+        console.log(a + b)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// Task 4: 
+/*
+If the passed-in arguments are not numbers, throw an error
+If either of the arguments passed to the addTwoNums are not numbers, you'll throw an error.
+
+Specifically, code a conditional with the following logic:
+
+if the typeof the a parameter is not equal to 'number', throw a new ReferenceError. Inside the ReferenceError, pass a custom error message of 'the first argument is not a number'.
+
+else if the typeof the b parameter is not equal to 'number', throw a new ReferenceError. Inside the ReferenceError, pass a custom error message of 'the second argument is not a number'.
+
+else, console log a + b  
+
+Once you've completed this task, all the code inside the try block will be inside these conditional statements.
+*/
+
+function addTwoNums(a, b) {
+
+    try {
+        if (typeof (a) !== 'number') {
+            throw new ReferenceError('the first argument is not a number');
+        } else if (typeof (b) !== 'number') {
+            throw new ReferenceError("the second argument is not a number")
+        } else {
+            console.log(a + b);
+        }
+    } catch (err) {
+        console.log("Error!", err);
+    }
+}
+addTwoNums(5,"5")
+addTwoNums("5",5)
