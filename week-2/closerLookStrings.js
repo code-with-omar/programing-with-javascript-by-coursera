30ce062fd9bbc87d20f2977bd95a7fafb3f3699a
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-11 16:33:52 +0600

    learn array iterable

---
+++ week-2/closerLookStrings.js
@@ -0,0 +1,7 @@
+//Array iterable
+var veggies = ['onion', 'parsley', 'carrot'];
+console.log(veggies[0]);
+console.log(veggies[1]);
+for (var i = 0; i < veggies.length; i++) {
+    console.log(veggies[i]);
+}
