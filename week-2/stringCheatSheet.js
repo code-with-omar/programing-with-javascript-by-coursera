af38d1daea021c0e8e304a05522b4f7779e7eee6
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-11 17:05:31 +0600

    Learn the String Function

---
+++ week-2/stringCheatSheet.js
@@ -0,0 +1,16 @@
+// toLowerCase()
+var greet = "Hello, ";
+var place = "World";
+console.log(greet.length);
+// charAt()
+console.log(greet.charAt());
+// concat()
+console.log("Wo".concat("rl").concat("d"));
+// indexOf()
+console.log("ho-ho-ho".indexOf('-'));
+// split()
+console.log("ho-ho-ho".split("-"));//make array
+// toUpperCase()
+console.log(greet.toUpperCase());
+// toLowerCase()
+console.log(greet.toLocaleLowerCase());
