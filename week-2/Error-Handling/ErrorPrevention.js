d387b38a0c352c04427de9a1b066fb82d819c066
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-11 21:33:28 +0600

    Complete task 1 and task 2 of Exercise: Error prevention

---
+++ week-2/Error-Handling/ErrorPrevention.js
@@ -0,0 +1,14 @@
+//Task 1: 
+/*
+Code a function declaration
+You need to code a function declaration named addTwoNums, which accepts numbers a and b and console logs a + b.
+*/
+function addTwoNums(a, b) {
+    console.log(a + b);
+}
+// Task 2: 
+/*
+Invoke the addTwoNums function with a number and a string
+You need to invoke the addTwoNums using the following arguments: 5 and "5".
+*/
+addTwoNums(5,"5");
