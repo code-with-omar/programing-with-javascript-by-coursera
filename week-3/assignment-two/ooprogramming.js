3f73ba1f4aac696cbd095439ffa4fdd198e28ee0
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-14 01:00:12 +0600

    Complete the task-2 of second assignment

--- week-3/assignment-two/ooprogramming.js
+++ week-3/assignment-two/ooprogramming.js
@@ -2,21 +2,30 @@
 class Person {
     constructor(name = "Tom", age = 20, energy = 100) {
         this.name = name;
-        this.age = name;
+        this.age = age;
         this.energy = energy;
     }
     sleep() {
-        this.energy -= 10;
+        this.energy += 10;
     }
     doSomethingFun() {
-        this.energy += 10;
+        this.energy -= 10;
     }
 }
 // Task 2: Code a Worker class
-
+class Worker extends Person {
+    constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+    goToWork() {
+        this.xp += xp;
+    }
+}
 // Task 3: Code an intern object, run methods
 function intern() {
-
+    new Worker(name)
 }
 
 // Task 4: Code a manager object, methods
