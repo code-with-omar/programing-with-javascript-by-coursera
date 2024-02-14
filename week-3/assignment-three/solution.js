9ab81d60e83503c0c8899011c816ec2207b07091
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-14 13:02:06 +0600

    Complete the task -1 of assignment-three

---
+++ week-3/assignment-three/solution.js
@@ -0,0 +1,13 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+function logDairy() {
+
+    for (const item of dairy) {
+        console.log(item);
+    }
+}
+logDairy();
+// Task 2
+
+// Task 3
+
