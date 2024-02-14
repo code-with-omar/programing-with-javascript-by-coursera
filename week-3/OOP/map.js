b99bd9c6a9dbf59544220e090237c0b175b77898
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-14 15:34:39 +0600

    to understand map function

---
+++ week-3/OOP/map.js
@@ -0,0 +1,11 @@
+// The map method
+const numbers = [4, 9, 16, 25];
+const newArr = numbers.map(Math.sqrt);
+console.log(newArr)
+const numbers1 = [65, 44, 12, 4];
+const newArr1 = numbers1.map(myFunction)
+
+function myFunction(num) {
+    return num * 10;
+}
+console.log(newArr1)
