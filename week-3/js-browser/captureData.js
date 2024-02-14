39ce3afaae3decb1332a6d36d270e8c476c0880b
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-15 02:30:04 +0600

    complete the Capture Data practice

---
+++ week-3/js-browser/captureData.js
@@ -0,0 +1,26 @@
+const h1 = document.querySelector("h1");
+const arr = ['Example Domain',
+    'First Click',
+    'Second Click',
+    'Third Click']
+
+function handleClicks() {
+    switch (h1.innerText) {
+        case arr[0]:
+            h1.innerText = arr[1];
+            // console.log(0)
+            break
+        case arr[1]:
+            h1.innerText = arr[2]
+            // console.log(1)
+            break
+        case arr[2]:
+            h1.innerText = arr[3]
+            // console.log(2)
+            break
+        default:
+            h1.innerText = arr[0]
+    }
+}
+
+h1.addEventListener('click', handleClicks);
