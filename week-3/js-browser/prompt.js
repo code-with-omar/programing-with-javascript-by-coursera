1ddb70e4b270466f502cab393ad1aad01c987ad4
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-15 02:07:19 +0600

    try to understand form in js by the alernative of prompt in js

--- week-3/js-browser/prompt.js
+++ week-3/js-browser/prompt.js
@@ -6,3 +6,12 @@ if (typeof (answer) === 'string') {
     document.body.innerText = "";
     document.body.appendChild(h1);
 }
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
