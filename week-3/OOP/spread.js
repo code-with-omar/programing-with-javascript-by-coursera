f168eb4b749fdaf1282ad76e07803f78ad0b9626
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-14 15:57:54 +0600

    Understand the spread operator in js

---
+++ week-3/OOP/spread.js
@@ -0,0 +1,20 @@
+const numbersOne = [1, 2, 3];
+const numbersTwo = [4, 5, 6];
+const numbersCombined = [...numbersOne, ...numbersTwo];
+console.log(numbersCombined);
+
+
+const myVehicle = {
+    brand: 'Ford',
+    model: 'Mustang',
+    color: 'red'
+}
+
+const updateMyVehicle = {
+    type: 'car',
+    year: 2021,
+    color: 'yellow'
+}
+//Notice the properties that did not match were combined, but the property that did match, color, was overwritten by the last object that was passed, updateMyVehicle. The resulting color is now yellow.
+const myUpdatedVehicle = { ...myVehicle, ...updateMyVehicle }
+console.log(myUpdatedVehicle)
