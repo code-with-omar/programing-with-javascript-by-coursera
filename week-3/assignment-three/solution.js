f06878d8f0c0608c0b42d2d3be392fbf10238a26
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-14 13:08:22 +0600

    Complete the task-2 of assignment three

--- week-3/assignment-three/solution.js
+++ week-3/assignment-three/solution.js
@@ -8,6 +8,22 @@ function logDairy() {
 }
 logDairy();
 // Task 2
+const animal = {
 
+    canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+function hasFeathers() {
+    for (const key of Object.keys(bird)) {
+        console.log(`${key}: ${bird[key]}`);
+    }
+}
+hasFeathers()
 // Task 3
 
