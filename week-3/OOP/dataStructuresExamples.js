48ac23df1282e9c1f71495e5dc0c5f593f51f151
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-14 15:18:36 +0600

    learn forEach() method

---
+++ week-3/OOP/dataStructuresExamples.js
@@ -0,0 +1,14 @@
+// the focus is on three specific methods that exist on arrays
+// 1.forEach 
+// 2.filter
+// 3.map
+const fruits = ["kiwi", "mango", "apple", "pear"];
+function appendIndex(fruit, index) {
+    console.log((`${index}. ${fruit}`));
+}
+fruits.forEach(appendIndex);
+
+const veggies = ['onion', 'garlic', 'potato'];
+veggies.forEach(function (veggie, index) {//first perametre value, 2nd index
+    console.log(`${index}. ${veggie}`);
+});
