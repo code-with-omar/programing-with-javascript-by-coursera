6e34b5a7364f766ac14e9e62d57c35daa7514ea1
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-15 10:16:53 +0600

    complete the final assignment task one

---
+++ week-5/final-assignment/finalProject.js
@@ -0,0 +1,46 @@
+// Given variables
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+const tax = 1.20;
+
+// Implement getPrices()
+function getPrices(taxBoolean) {
+    for (let i = 0; i < dishData.length; i++) {
+        let finalPrice;
+        if (taxBoolean===true) {
+            finalPrice = dishData[i].price * tax;
+
+        } else if (taxBoolean === false) {
+            finalPrice = dishData[i].price;
+
+        } else {
+            console.log("You need to pass a boolean to the getPrices call!");
+            return
+        }
+        console.log(`Dish: ${dishData[i].name} Price: ${dishData[i].price} finalPrice: $${finalPrice}`);
+    }
+}
+getPrices()
+
+// Implement getDiscount()
+function getDiscount() {
+
+}
+
+// Call getDiscount()
