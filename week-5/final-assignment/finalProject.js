94bca54af749aa55671962b256d5eac2c192fb3d
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-15 10:35:23 +0600

    complete the final assignment task-2

--- week-5/final-assignment/finalProject.js
+++ week-5/final-assignment/finalProject.js
@@ -21,26 +21,40 @@ const tax = 1.20;
 
 // Implement getPrices()
 function getPrices(taxBoolean) {
-    for (let i = 0; i < dishData.length; i++) {
+    for (let dish of dishData) {
         let finalPrice;
-        if (taxBoolean===true) {
-            finalPrice = dishData[i].price * tax;
 
-        } else if (taxBoolean === false) {
-            finalPrice = dishData[i].price;
-
+        if (taxBoolean == true) {
+            finalPrice = dish.price * tax;
+        } else if (taxBoolean == false) {
+            finalPrice = dish.price;
         } else {
-            console.log("You need to pass a boolean to the getPrices call!");
+            console.log("You need to pass a boolean to the getPrices call!")
             return
-        }
-        console.log(`Dish: ${dishData[i].name} Price: ${dishData[i].price} finalPrice: $${finalPrice}`);
+        };
+        console.log(`Dish: ${dish.name} Price: $${finalPrice}` )
     }
 }
-getPrices()
-
+getPrices(false)
 // Implement getDiscount()
-function getDiscount() {
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
+
+    if (typeof (guests) === "number" && (guests > 0 && guests < 30)) {
+        let discount = 0;
+        if (guests < 5) {
+            discount = 5;
+        }
+        else if (guests >= 5) {
+            discount = 10;
+        }
+        console.log(`Discount is: $` + discount);
+    } else {
+        console.log(`The second argument must be a number between 0 and 30`)
+    }
 
 }
 
 // Call getDiscount()
+getDiscount(true, 2);
+getDiscount(false, 10)
